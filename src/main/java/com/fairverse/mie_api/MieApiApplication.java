2f6f3f01991a81dc9fafb20519a7f1c94552124b
U180709061 <hakkicanbuluc@gmail.com>
2022-07-18 22:06:15 +0300

    initial commit

---
+++ src/main/java/com/fairverse/mie_api/MieApiApplication.java
@@ -0,0 +1,13 @@
+package com.fairverse.mie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fairverse/mie_api/MieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairverse.mie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
